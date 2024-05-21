a383cdeb49b39dc42f529a3f72cf07057a7b8c56
Esten Rye <esten.rye@ryezone.com>
2024-05-21 13:47:59 +0000

    remove unused variable.

--- cloudspace-kubeconfig/variables.tf
+++ cloudspace-kubeconfig/variables.tf
@@ -1,8 +1,4 @@
-variable "BUCKET_NAME" {
-    type = string
-}
-
-variable "cloudspace_name" {
+ variable "cloudspace_name" {
     description = "Name of the Rackspace Spot Cloudspace"
     type        = string
     default     = "example"
