681dd926e7b3273d1ae4eb9a3cabdbede9da8936
Esten Rye <esten@platform9.com>
2024-07-01 18:15:33 -0500

    add lifecycle rules to satisfy CKV2_AWS_61

--- backend-aws-s3/s3-access-logging-bucket.tf
+++ backend-aws-s3/s3-access-logging-bucket.tf
@@ -89,3 +89,34 @@ data "aws_iam_policy_document" "s3_access_logging_bucket_policy" {
     }
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "logging_bucket" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.logging_bucket]
+
+  bucket = aws_s3_bucket.logging_bucket.id
+
+  rule {
+    id = "versioning-rule"
+
+    expiration {
+        days = 730
+    }
+    
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
+}
--- backend-aws-s3/tf-state-bucket.tf
+++ backend-aws-s3/tf-state-bucket.tf
@@ -53,3 +53,30 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_bucket_s
     }
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "tf_state_bucket" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.tf_state_bucket]
+
+  bucket = aws_s3_bucket.tf_state_bucket.id
+
+  rule {
+    id = "versioning-rule"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
+}
