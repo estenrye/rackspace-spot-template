9b795eaa5dfe31b65cbb70ee43fe47e95f9f4742
Esten Rye <esten@platform9.com>
2024-07-01 17:06:05 -0500

    add bucket policy for logging

--- backend-aws-s3/s3-access-logging-bucket.tf
+++ backend-aws-s3/s3-access-logging-bucket.tf
@@ -45,3 +45,45 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logging_bucket" {
     }
   }
 }
+
+resource "aws_s3_bucket_policy" "logging_bucket" {
+  bucket = aws_s3_bucket.logging_bucket.bucket
+  policy = data.aws_iam_policy_document.s3_access_logging_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "s3_access_logging_bucket_policy" {
+  statement {
+    sid = "S3ServerAccessLogsPolicy"
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    principals {
+      type       = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+
+    resources = [
+      "${aws_s3_bucket.logging_bucket.arn}/${var.bucket_name}/*"
+    ]
+
+    condition {
+        test     = "ArnLike"
+        variable = "aws:SourceArn"
+        values   = [
+            "arn:aws:s3:::${var.bucket_name}"
+        ]
+    }
+
+    condition {
+        test     = "StringEquals"
+        variable = "aws:SourceAccount"
+        values   = [
+            data.aws_caller_identity.current.account_id
+        ]
+    }
+  }
+}
+
