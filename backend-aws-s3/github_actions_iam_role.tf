7c8e22c9fead150ce7cadfef82132f69791b23a1
Esten Rye <esten@platform9.com>
2024-07-04 00:04:24 -0500

    refactor IAM role to improve idempotence

--- backend-aws-s3/github_actions_iam_role.tf
+++ backend-aws-s3/github_actions_iam_role.tf
@@ -1,90 +1,100 @@
-resource "aws_iam_role" "github_actions_iam_role" {
-  name = "GitHubActionsIAMRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github_actions_oidc_provider.arn
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "${aws_iam_openid_connect_provider.github_actions_oidc_provider.url}:aud" = "sts.amazonaws.com"
-          },
-          StringLike = {
-            "${aws_iam_openid_connect_provider.github_actions_oidc_provider.url}:sub" = "repo:${var.github_orgname}/${var.github_repo}:*"
-          }
-        }
-      }
+data "aws_iam_policy_document" "github_actions_iam_assume_role_policy" {
+  statement {
+    sid    = "Allow Assume Role with Web Identity"
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions_oidc_provider.arn]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = "${aws_iam_openid_connect_provider.github_actions_oidc_provider.url}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "${aws_iam_openid_connect_provider.github_actions_oidc_provider.url}:sub"
+      values   = ["repo:${var.github_orgname}/${var.github_repo}:*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_iam_policy" {
+  statement {
+    sid    = "Allow S3 Object Access for TF State Bucket"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket",
+      "s3:DeleteObject"
     ]
-  })
-
-  inline_policy {
-    name = "GitHubActionsIAMRolePolicy"
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Effect = "Allow",
-          Action = [
-            "s3:GetObject",
-            "s3:PutObject",
-            "s3:ListBucket",
-            "s3:DeleteObject"
-          ],
-          Resource = [
-            aws_s3_bucket.tf_state_bucket.arn,
-            "${aws_s3_bucket.tf_state_bucket.arn}/*",
-          ]
-        },
-        {
-          Effect = "Allow",
-          Action = [
-            "iam:CreateOpenIDConnectProvider",
-            "iam:GetOpenIDConnectProvider",
-            "iam:TagOpenIDConnectProvider"
-          ],
-          Resource = [
-            aws_iam_openid_connect_provider.github_actions_oidc_provider.arn
-          ]
-        },
-        {
-          Effect = "Allow",
-          Action = [
-            "kms:CreateKey",
-            "kms:TagResource",
-            "iam:GetRole*",
-            "iam:ListRole*",
-            "iam:ListAttachedRolePolicies",
-            "iam:DeleteRolePolicy",
-            "iam:PutRolePolicy"
-          ],
-          Resource = ["*"]
-        },
-        {
-          Effect = "Allow",
-          Action = [
-            "s3:CreateBucket",
-            "s3:ListBucket",
-            "s3:GetBucket*",
-            "s3:GetAccelerateConfiguration",
-            "s3:GetLifecycleConfiguration",
-            "s3:GetReplicationConfiguration",
-            "s3:GetEncryptionConfiguration"
-          ],
-          Resource = [
-            aws_s3_bucket.logging_bucket.arn,
-            aws_s3_bucket.tf_state_bucket.arn
-          ]
-        },
-      ]
-    })
+    resources = [
+      aws_s3_bucket.tf_state_bucket.arn,
+      "${aws_s3_bucket.tf_state_bucket.arn}/*",
+    ]
   }
 
-  tags = {
-    Name    = "GitHub Actions IAM Role"
-    Project = "terraform-state-storage"
+  statement {
+    sid    = "Allow OIDC Provider Management"
+    effect = "Allow"
+    actions = [
+      "iam:CreateOpenIDConnectProvider",
+      "iam:GetOpenIDConnectProvider",
+      "iam:TagOpenIDConnectProvider"
+    ]
+    resources = [
+      aws_iam_openid_connect_provider.github_actions_oidc_provider.arn
+    ]
   }
+
+  statement {
+    sid    = "Allow KMS Key and IAM Role Management"
+    effect = "Allow"
+    actions = [
+      "kms:CreateKey",
+      "kms:TagResource",
+      "iam:GetRole*",
+      "iam:ListRole*",
+      "iam:ListAttachedRolePolicies",
+      "iam:DeleteRolePolicy",
+      "iam:PutRolePolicy"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow Bucket Management"
+    effect = "Allow"
+    actions = [
+      "s3:CreateBucket",
+      "s3:ListBucket",
+      "s3:GetBucket*",
+      "s3:GetAccelerateConfiguration",
+      "s3:GetLifecycleConfiguration",
+      "s3:GetReplicationConfiguration",
+      "s3:GetEncryptionConfiguration"
+    ]
+    resources = [
+      aws_s3_bucket.logging_bucket.arn,
+      aws_s3_bucket.tf_state_bucket.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "github_actions_iam_policy" {
+  name        = "github-actions-iam-policy"
+  description = "IAM policy for GitHub Actions"
+  policy      = data.aws_iam_policy_document.github_actions_iam_policy.json
+}
+
+resource "aws_iam_role" "github_actions_iam_role" {
+  name               = "github-actions-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_iam_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_iam_policy_attachment" {
+  role       = aws_iam_role.github_actions_iam_role.name
+  policy_arn = aws_iam_policy.github_actions_iam_policy.arn
 }
